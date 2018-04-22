47e4f570271094ee6a5983f7bfc7b9ab1ed52ceb
Jussi Liukkonen <jussi.liukkonen95@gmail.com>
2018-04-23 00:06:52 +0300

    First commit

---
+++ bot.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+const auth = require("./auth.json");
+
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
+  }
+});
+
+client.login(auth.token);
