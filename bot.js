fff3cb5a350ca1b2e5dbec1de1952f0148b4f6dc
talkkitee <talkkitee@gmail.com>
2018-05-04 23:52:09 +0300

    Change formatting

--- bot.js
+++ bot.js
@@ -3,129 +3,129 @@ const auth = require("./auth.json");
 
 const client = new Discord.Client();
 // Reaction numbers as Unicode, reacting with them normally doesn't work
-var reaction_numbers = ["\u0030\u20E3","\u0031\u20E3","\u0032\u20E3","\u0033\u20E3","\u0034\u20E3","\u0035\u20E3", "\u0036\u20E3","\u0037\u20E3","\u0038\u20E3","\u0039\u20E3"];
+var reaction_numbers = ["\u0030\u20E3", "\u0031\u20E3", "\u0032\u20E3", "\u0033\u20E3", "\u0034\u20E3", "\u0035\u20E3", "\u0036\u20E3", "\u0037\u20E3", "\u0038\u20E3", "\u0039\u20E3"];
 
 client.on("ready", () => {
-	console.log("I am ready!");
-	client.user.setUsername("MunchBot");
+    console.log("I am ready!");
+    client.user.setUsername("MunchBot");
 });
 
 // Triggered when message is sent in server
 client.on("message", (message) => {
 
-	// Prevent loop from bots own messages
-        if(message.author.bot) return;
+    // Prevent loop from bots own messages
+    if (message.author.bot) return;
 
-	// Get command and its arguments
-	if(message.content.substring(0, 1) == "!"){
-	        const args = message.content.slice(1).trim().split(" ");
-        	const command = args.shift().toLowerCase();
+    // Get command and its arguments
+    if (message.content.substring(0, 1) == "!") {
+        const args = message.content.slice(1).trim().split(" ");
+        const command = args.shift().toLowerCase();
 
-		// Creating a raid
-		if(command === "tr"){
+        const cmd = parseCommand(message.content)
+
+        // Creating a raid
+        if (command === "tr") {
             // test
-            /*
-            try {
-                cmd = parseCommand(message.content)
-                console.log('COMMAND: ' + cmd)
-                raid = parseRaid(message.content)
-                console.log(raid)
-                msg2 = raidToMessage(raid)
-                console.log(msg2)
-            } catch (e) {
-                console.log(e)
-            }
-            */
-	                const boss = args[0];
-       	        	const time = args[1];
-                	let gym = "";
-                	let i;
-                	for(i = 2; i < args.length; i++){
-                	        gym = gym + args[i] + " ";
-                	}
-                	const msg = "```Boss: " + boss + "\nTime: " + time + "\nLocation: " + gym + "\n-------------------------\nIlmoittautuneet: \nYhteensä: 0```";
-                	message.delete().catch(O_o=>{});
-                	message.channel.send(msg)
-				.then(function(message){
-	        	                message.react("438598806825861131" /*reaction_numbers[1]*/);
-	        	                message.react("438599265283997706" /*reaction_numbers[2]*/);
-	        	                message.react("438599716863868928" /*reaction_numbers[3]*/);
-				});
-		}
 
-		// PingPong!
-        	if (command === "ping") {
-        	        message.channel.send("pong!`" + (new Date().getTime() - message.createdTimestamp) + "ms`");
-        	}
-	}
+
+            console.log('COMMAND: ' + cmd)
+            raid = parseRaid(message.content)
+            console.log(raid)
+            msg2 = raidToMessage(raid)
+            console.log(msg2)
+
+
+            const boss = args[0];
+            const time = args[1];
+            let gym = "";
+            let i;
+            for (i = 2; i < args.length; i++) {
+                gym = gym + args[i] + " ";
+            }
+            const msg = "```Boss: " + boss + "\nTime: " + time + "\nLocation: " + gym + "\n-------------------------\nIlmoittautuneet: \nYhteensä: 0```";
+            message.delete().catch(O_o=> {
+            });
+            message.channel.send(msg2)
+                .then(function (message) {
+                    message.react(/* "438598806825861131" */ reaction_numbers[1]);
+                    message.react(/* "438599265283997706" */ reaction_numbers[2]);
+                    message.react(/* "438599716863868928" */ reaction_numbers[3]);
+                });
+        }
+
+        // PingPong!
+        if (command === "ping") {
+            message.channel.send("pong!`" + (new Date().getTime() - message.createdTimestamp) + "ms`");
+        }
+    }
 });
 
 function editMessage(reaction, user) {
 
-	if(user.bot) return;
-        if(reaction.message.author.id !== "437713794576285696") return;
+    if (user.bot) return;
+    if (reaction.message.author.id !== "437713794576285696") return;
 
-        let newMessageContent = "";
-        let msgParts = reaction.message.content.split("Ilmoittautuneet:");
+    let newMessageContent = "";
+    let msgParts = reaction.message.content.split("Ilmoittautuneet:");
 
-        // -6, so we don't count bot's own reactions!
-        let playerCount = -6;
-        let users = new Map();
+    // -6, so we don't count bot's own reactions!
+    let playerCount = -6;
+    let users = new Map();
 
-        for (var [key, value] of reaction.message.reactions) {
-		console.log(key);
-                if(key === "3_:438599716863868928" /*reaction_numbers[3]*/){
-                        for(var [ukey, uvalue] of value.users){
-                               	if(!users.has(uvalue.username)){
-                                        users.set(uvalue.username, 2);
-                                }else{
-                                      	users.set(uvalue.username, users.get(uvalue.username) + 3);
-                                }
-                         }
-                        playerCount += value.count * 3;
-                } else if(key === "2_:438599265283997706" /*reaction_numbers[2]*/){
-                        for(var [ukey, uvalue] of value.users){
-                                if(!users.has(uvalue.username)){
-                                        users.set(uvalue.username, 1);
-                                }else{
-                                      	users.set(uvalue.username, users.get(uvalue.username) + 2);
-                                }
-                         }
-                        playerCount += value.count * 2;
-                } else if(key === "1_:438598806825861131" /*reaction_numbers[1]*/){
-                        for(var [ukey, uvalue] of value.users){
-                                if(!users.has(uvalue.username)){
-                                        users.set(uvalue.username, 0);
-                                }else{
-                                      	users.set(uvalue.username, users.get(uvalue.username) + 1);
-                                }
-                         }
-                        playerCount += value.count;
+    for (var [key, value] of reaction.message.reactions) {
+        console.log(key);
+        if (key === "3_:438599716863868928" /*reaction_numbers[3]*/) {
+            for (var [ukey, uvalue] of value.users) {
+                if (!users.has(uvalue.username)) {
+                    users.set(uvalue.username, 2);
+                } else {
+                    users.set(uvalue.username, users.get(uvalue.username) + 3);
                 }
-        }
-
-        let usernamelist = "";
-        for (var [key, value] of users) {
-                if(key !== "MunchBot") {
-                        if(value != 0){
-                                usernamelist += "(" + (value + 1) + ") " + key + " +" + value + "\n";
-                        }else{
-                              	usernamelist += "(" + (value + 1) + ") " + key + "\n";
-                        }
+            }
+            playerCount += value.count * 3;
+        } else if (key === "2_:438599265283997706" /*reaction_numbers[2]*/) {
+            for (var [ukey, uvalue] of value.users) {
+                if (!users.has(uvalue.username)) {
+                    users.set(uvalue.username, 1);
+                } else {
+                    users.set(uvalue.username, users.get(uvalue.username) + 2);
                 }
+            }
+            playerCount += value.count * 2;
+        } else if (key === "1_:438598806825861131" /*reaction_numbers[1]*/) {
+            for (var [ukey, uvalue] of value.users) {
+                if (!users.has(uvalue.username)) {
+                    users.set(uvalue.username, 0);
+                } else {
+                    users.set(uvalue.username, users.get(uvalue.username) + 1);
+                }
+            }
+            playerCount += value.count;
         }
-	newMessageContent = msgParts[0] + "Ilmoittautuneet:\n" + usernamelist + "\nYhteensä: " + playerCount + "```";
+    }
 
-        reaction.message.edit(newMessageContent);
+    let usernamelist = "";
+    for (var [key, value] of users) {
+        if (key !== "MunchBot") {
+            if (value != 0) {
+                usernamelist += "(" + (value + 1) + ") " + key + " +" + value + "\n";
+            } else {
+                usernamelist += "(" + (value + 1) + ") " + key + "\n";
+            }
+        }
+    }
+    newMessageContent = msgParts[0] + "Ilmoittautuneet:\n" + usernamelist + "\nYhteensä: " + playerCount + "```";
+
+    reaction.message.edit(newMessageContent);
 
 }
 
 client.on("messageReactionAdd", (reaction, user) => {
-	editMessage(reaction, user);
+    editMessage(reaction, user);
 });
 
 client.on("messageReactionRemove", (reaction, user) => {
-	editMessage(reaction, user);
+    editMessage(reaction, user);
 });
 
 function parseCommand(commandLine) {
@@ -154,7 +154,7 @@ function parseRaid(commandLine) {
         boss: parts[1],
         time: parts[2],
         gym: parts[3],
-        users: {}
+        users: []
     }
 }
 
@@ -162,14 +162,20 @@ function messageToRaid(message) {
 
 }
 function raidToMessage(raid) {
-    msg = '```'
-    msg += 'Boss: ' + raid.boss + '\n'
-    msg += 'Time: *' + raid.time + '*\n'
-    msg += 'Location: ' + raid.location + '\n'
-    msg += 'Trainers: ' + JSON.stringify(raid.users) + '\n'
-    msg = '```'
+    msg = '```' +
+        'Boss: ' + raid.boss + '\n' +
+        'Time: ' + raid.time + '\n' +
+        'Location: ' + raid.gym + '\n' +
+        'Trainers: ' + formatTrainerList(raid.users) + '\n' +
+        '```'
     return msg
 }
 
+function formatTrainerList(users) {
+    if (users.length == 0) {
+        return "none"
+    }
+    return users.sort().join(', ') + '\nTOTAL: ' + users.length
+}
 
 client.login(auth.token);
